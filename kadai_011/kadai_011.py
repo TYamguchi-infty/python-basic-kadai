d2c3acdfd39f924a1482e39040fec2308e329f57
TYamaguchi-infty <takuto.yamaguchi.913@gmail.com>
2024-04-08 12:54:49 +0900

    kadai_011フォルダを追加

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,11 @@
+array = ["水","金", "地", "火", "木", "土", "天", "海", "冥"]
+
+#for文を使用
+for item in array:
+    print(item)
+    
+#while文を使用
+cnt = 0
+while cnt < len(array):
+    print(array[cnt])
+    cnt += 1
