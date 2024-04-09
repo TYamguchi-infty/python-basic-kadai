8b93b1004760ef2c19889e6e9ec417cf298dec21
TYamaguchi-infty <takuto.yamaguchi.913@gmail.com>
2024-04-09 13:09:49 +0900

    kadai_015を修正

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -1,12 +1,15 @@
 class Human():
-    def __init__(self):
-        self.name = "Taro"
-        self.age = 25
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
         
     def printinfo(self):
         print(f"名前は{self.name}です")
         print(f"年齢は{self.age}歳です")
-        
-human = Human()
+
+#インスタンス化
+name = "Taro"
+age = 26
+human = Human(name, age)
 
 human.printinfo()
