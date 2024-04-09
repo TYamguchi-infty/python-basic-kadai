8f7b8e45d2edbc6adcc60fd2c6bb1a14b91b3303
TYamaguchi-infty <takuto.yamaguchi.913@gmail.com>
2024-04-09 13:06:55 +0900

    kadai_013を再提出用に修正

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,9 +1,10 @@
 #関数の定義
 def calc_consumption_tax(value, tax):
     total = value * (1 + tax/100)
-    print(total)
+    # print(total)
+    return total
 
 value = 100
 tax = 10 #[%]
 
-calc_consumption_tax(value, tax)
+print(calc_consumption_tax(value, tax))
